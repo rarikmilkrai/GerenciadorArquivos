3d8a08a1ef3f8dc2613489221e0a17cf16cfbdf7
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2022-11-06 17:27:34 -0300

    feito junto com o youtube.

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import os
+import shutil
+from PySide2.QtWidgets import *
+from PySide2.QtGui import QIcon
