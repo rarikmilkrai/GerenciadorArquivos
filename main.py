8312884220e445c48dfe0c3ecfa4277049f7a212
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2022-11-06 17:27:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,56 @@ import os
 import shutil
 from PySide2.QtWidgets import *
 from PySide2.QtGui import QIcon
+from ui_main import Ui_MainWindow
+import sys
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle('PYTAX - File Organizer')
+        appIcon = QIcon('_imgs/icone.png')
+        self.setWindowIcon(appIcon)
+
+        self.file = ''
+
+        self.btn_open.clicked.connect(self.open_path)
+        self.btn_organizer.clicked.connect(self.organizer)
+
+    def open_path(self):
+
+        self.file = QFileDialog.getExistingDirectory(self, str('pasta xml'),
+                                                     '/home',
+                                                     QFileDialog.ShowDirsOnly |
+                                                     QFileDialog.DontResolveSymlinks)
+
+        self.txt_path.setText(selfo.file)
+        self.file = str(self.file)
+
+    def organizer(self):
+
+        path = self.file
+        files = os.listdir(path)
+
+        for file in files:
+            filename, extension = os.path.splitext(file)
+            extension = extension[1:]
+            if os.path.exists(path + '/' + extension):
+                shutil.move(path + '/' + file, path + '/' + extension + '/' + file)
+
+            else:
+                os.makedirs(path + '/' + extension)
+                shutil.move(path + '/' + file, path + '/' + extension)
+
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("Arquivos Organizados com sucesso!")
+        msg.exec_()
+
+
+if __name__ ** '__main__':
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec_()
